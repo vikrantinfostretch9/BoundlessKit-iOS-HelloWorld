98b2de0ed24dbd004d9c92c8a5f25f6390bf7401
Akash Desai <kash650@gmail.com>
2016-06-02 14:48:36 -0700

    imported using cocoapods

---
+++ DopamineDummy Swift/Pods/DopamineKit/DopamineKit/Frameworks/DopamineKit.framework/Headers/DopamineKit.h
@@ -0,0 +1,17 @@
+//
+//  DopamineKit.h
+//  DopamineKit
+//
+//  Created by Akash Desai on 4/15/16.
+//  Copyright © 2016 DopamineLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DopamineKit.
+FOUNDATION_EXPORT double DopamineKitVersionNumber;
+
+//! Project version string for DopamineKit.
+FOUNDATION_EXPORT const unsigned char DopamineKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DopamineKit/PublicHeader.h>
